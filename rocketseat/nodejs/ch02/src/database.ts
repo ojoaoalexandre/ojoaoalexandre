6073530aedac40269eb24c8de6371f2c455c9181
Alexandre Bekor <alexandre@agenciabekor.com>
2023-07-28 16:33:38 -0300

    add .env

--- rocketseat/nodejs/ch02/src/database.ts
+++ rocketseat/nodejs/ch02/src/database.ts
@@ -1,9 +1,19 @@
+import "dotenv/config";
 import { Knex, knex } from "knex";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE URL not found");
+}
 
 export const config: Knex.Config = {
   client: "sqlite3",
   connection: {
-    filename: "./tmp/app.db",
+    filename: process.env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
   },
 };
 
--- rocketseat/nodejs/ch02/src/server.ts
+++ rocketseat/nodejs/ch02/src/server.ts
@@ -1,12 +1,18 @@
 import fastify from "fastify";
 import { database } from "./database";
+import { randomUUID } from "crypto";
 
 const app = fastify();
 
 app.get("/users", async () => {
-  const test = await database("sqlite_schema").select("*");
+  await database("transactions").insert({
+    id: randomUUID(),
+    description: "Primeiro registro",
+    amount: 10.2,
+  });
 
-  return test;
+  const transaction = database("transactions").select("*");
+  return transaction;
 });
 
 try {
