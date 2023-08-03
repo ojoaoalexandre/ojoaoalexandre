0a87e445fffe6573247d42383c7e94c57536583a
Alexandre Bekor <alexandre@agenciabekor.com>
2023-08-02 23:02:55 -0300

    add test to get report and delete transaction

--- rocketseat/nodejs/ch02/test/transactions.test.ts
+++ rocketseat/nodejs/ch02/test/transactions.test.ts
@@ -59,6 +59,36 @@ describe("Transactions Routes", () => {
     );
   });
 
+  it("should be able to get a report with the sum of transactions", async () => {
+    const createCreditTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        description: "Transaction 1",
+        amount: 8500,
+        type: "credit",
+      });
+
+    const cookies = createCreditTransactionResponse.get("Set-Cookie");
+
+    const createDebitTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .set("Cookie", cookies)
+      .send({
+        description: "Transaction 2",
+        amount: 1000,
+        type: "debit",
+      });
+
+    const response = await request(app.server)
+      .get("/transactions/report")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(response.body.report).toEqual({
+      amount: 7500,
+    });
+  });
+
   // it.skip("should be ..."...) -> Pular o teste
   // it.todo("should be ..."...) -> Para quando queremos apenas listar para lembrar de fazer o teste
   // it.only("should be ..."...) -> Executa apenas esse teste
@@ -84,5 +114,27 @@ describe("Transactions Routes", () => {
         amount: 8500,
       }),
     ]);
+  });
+
+  it("should be able to delete a specific transaction", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        description: "Transaction",
+        amount: 8000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const getTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+    const transactionId = getTransactionsResponse.body.transactions[0].id;
+
+    await request(app.server)
+      .delete(`/transactions/${transactionId}`)
+      .expect(200);
   });
 });
